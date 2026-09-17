12b8f44f4e5abb4fb22cbe1e714caa016e29947d
Ahmed <https://github.com/ARSHAH00/my-auth-project.git>
2026-09-17 18:25:28 +0500

    Fix duplicate port declaration

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 // =========================
 // MIDDLEWARE
 // =========================
