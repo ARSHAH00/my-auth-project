d6e87516700fdf887cfc983773eab36bb967552d
Ahmed <https://github.com/ARSHAH00/my-auth-project.git>
2026-09-17 18:20:21 +0500

    Fix deployment port

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,10 @@ app.use("/api/auth", authRoutes);
 
 // =========================
 // START SERVER
-// =========================
+// ========================
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on 0.0.0.0:${PORT}`);
 });
